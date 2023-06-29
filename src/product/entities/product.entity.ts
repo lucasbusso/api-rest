47bb2f816f65c0a67c8c5a9ac67f1ac72e30b82f
Lucas Busso <lucas.busso.cft@jcrew.com>
2023-06-28 21:10:37 -0300

    Product: entity update, dto, controller, service and router

--- src/product/entities/product.entity.ts
+++ src/product/entities/product.entity.ts
@@ -1,23 +1,27 @@
-import { Entity, Column, ManyToOne, JoinColumn, OneToMany } from "typeorm";
+import { Column, Entity, ManyToOne, JoinColumn, OneToMany } from "typeorm";
 import { BaseEntity } from "../../config/base.entity";
+import { CustomerEntity } from "../../customer/entities/customer.entity";
 import { CategoryEntity } from "../../category/entities/category.entity";
-import { PurchaseProductEntity } from "../../purchase/entities/purchases-products.entity";
+import { PurchaseProductEntity } from "../..//purchase/entities/purchases-products.entity";
 
-@Entity({name:"product"})
+@Entity({ name: "product" })
 export class ProductEntity extends BaseEntity {
-    @Column()
-    productName!: string;
+  @Column()
+  productName!: string;
 
-    @Column()
-    description!: string;
+  @Column()
+  description!: string;
 
-    @Column()
-    price!: number;
+  @Column()
+  price!: number;
 
-    @ManyToOne(() => CategoryEntity, (category) => category.products)
-    @JoinColumn({name:"category_id",})
-    category!: CategoryEntity;
+  @ManyToOne(() => CategoryEntity, (category) => category.products)
+  @JoinColumn({ name: "category_id" })
+  category!: CategoryEntity;
 
-    @OneToMany(() => PurchaseProductEntity, (purchaseProduct) => purchaseProduct.product)
-    purchaseProduct!: PurchaseProductEntity[];
-}   
+  @OneToMany(
+    () => PurchaseProductEntity,
+    (purchaseProduct) => purchaseProduct.product
+  )
+  purchaseProduct!: PurchaseProductEntity[];
+}
